081d04dd799872319884094e64702e9862a7a315
FeelingAlive0 <feelingalive.com@gmail.com>
2025-07-22 09:19:49 +0200

    Delete src/main directory

--- src/main/java/feelingalive/advancedcaptcha/captcha/CaptchaType.java
+++
@@ -1,7 +0,0 @@
-package feelingalive.advancedcaptcha.captcha;
-
-public enum CaptchaType {
-    BUTTON,
-    MATH,
-    RANDOM
-}
--- src/main/java/feelingalive/advancedcaptcha/captcha/CaptchaTypeHandler.java
+++
@@ -1,8 +0,0 @@
-package feelingalive.advancedcaptcha.captcha;
-
-import org.bukkit.entity.Player;
-
-public interface CaptchaTypeHandler {
-    void showCaptcha(Player player, CaptchaSession session);
-    boolean verify(Player player, CaptchaSession session, int selectedButton);
-}
